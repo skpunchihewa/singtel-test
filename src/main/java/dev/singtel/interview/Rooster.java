390ec3c5ddc35fbc91b68cd6ca5d506634477255
Thusitha Nuwan <tmnuwan12@gmail.com>
2018-10-27 23:10:49 +0800

    Answer question 3a, 3b

---
+++ src/main/java/dev/singtel/interview/Rooster.java
@@ -0,0 +1,11 @@
+package dev.singtel.interview;
+
+/**
+ * @author Thusitha Nuwan
+ */
+public class Rooster extends Chicken{
+
+    void sing(){
+        Utility.printLn("“Cock-a-doodle-doo”");
+    }
+}
